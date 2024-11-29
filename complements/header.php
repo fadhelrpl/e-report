c09392fb8eb5fcb8201994b3088e5aee1d14f161
fadhelrpl <mohamadfadhelrpl@gmail.com>
2024-11-29 14:29:43 +0700

    Update header.php

--- complements/header.php
+++ complements/header.php
@@ -3,16 +3,19 @@
     <div class="collapse navbar-collapse">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-            <?php $base_url = "/"; ?>
-            <a class="nav-link" href="<?php echo $base_url; ?>e-report/index.php">Beranda</a>
+                <?php 
+                    // Menetapkan base URL secara dinamis berdasarkan path server
+                    $base_url = dirname($_SERVER['SCRIPT_NAME']) . '/'; 
+                ?>
+                <a class="nav-link" href="<?php echo $base_url; ?>e-report/index.php">Beranda</a>
             </li>
             <li class="nav-item">
-            <?php $base_url = "/"; ?>
-            <a class="nav-link" href="<?php echo $base_url; ?>e-report/crud/create.php">Buat Pengaduan</a>
+                <?php $base_url = dirname($_SERVER['SCRIPT_NAME']) . '/'; ?>
+                <a class="nav-link" href="<?php echo $base_url; ?>e-report/crud/create.php">Buat Pengaduan</a>
             </li>
             <li class="nav-item">
-            <?php $base_url = "/"; ?>
-            <a class="nav-link" href="<?php echo $base_url; ?>e-report/auth/logout.php" onclick="return confirm('Yakin ingin logout?')">Logout</a>
+                <?php $base_url = dirname($_SERVER['SCRIPT_NAME']) . '/'; ?>
+                <a class="nav-link" href="<?php echo $base_url; ?>e-report/auth/logout.php" onclick="return confirm('Yakin ingin logout?')">Logout</a>
             </li>
         </ul>
     </div>
